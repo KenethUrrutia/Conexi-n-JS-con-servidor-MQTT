e5a723a266fab7d4636ba489bf0f4441e488780d
KenethUrrutia <106717176+KenethUrrutia@users.noreply.github.com>
2023-03-14 11:07:29 -0600

    s

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+const client  = mqtt.connect('ws://172.17.0.2:8083/mqtt')
+
+client.on('connect', function () {
+  client.subscribe('presence', function (err) {
+    if (!err) {
+      client.publish('presence', 'Hello mqtt')
+    }
+  })
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
